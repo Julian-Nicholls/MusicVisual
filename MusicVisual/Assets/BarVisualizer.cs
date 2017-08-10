846e21a89019bc80aeda1de83e328a6fafd0c7a1
Julian Nicholls <jnicholls95@outlook.com>
2017-08-10 11:47:48 -0400

    Created Simple Visualizers
    Although they technically cheat by changing sample rates, not by identifying frequencies

---
+++ MusicVisual/Assets/BarVisualizer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarVisualizer : MonoBehaviour {
+
+	public int detail;
+	public float amplitude;
+	Vector3 startScale;
+
+	// Use this for initialization
+	void Start () {
+
+		startScale = transform.localScale;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float[] info = new float[detail];
+
+		AudioListener.GetOutputData (info, 0);
+		float packagedData = 0f; 
+
+		for (int x = 0; x < info.Length; x++) {
+			packagedData = packagedData + System.Math.Abs (info [x]);
+		}
+
+		transform.localScale = startScale + new Vector3 (0, packagedData * amplitude, 0);
+	}
+}
---
+++ MusicVisual/Assets/Scripts/lineVisualizer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineVisualizer : MonoBehaviour {
+
+	public int detail;
+	public float amplitude;
+	float startPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+		startPosition = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float[] info = new float[detail];
+
+		AudioListener.GetOutputData (info, 0);
+		float packagedData = 0f; 
+
+		for (int x = 0; x < info.Length; x++) {
+			packagedData = packagedData + System.Math.Abs (info [x]);
+		}
+
+		transform.position = new Vector3 (0, startPosition + packagedData * amplitude, 0);
+	}
+}
